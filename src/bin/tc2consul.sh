17b53ab21adb6ee800da95340455c627c0801e7c
Alexander Lindhorst <alindhorst@ebay-kleinanzeigen.de>
2016-01-03 19:46:47 +0100

    Consul pushed to containers as nameservice

--- src/bin/tc2consul.sh
+++ src/bin/tc2consul.sh
@@ -6,7 +6,7 @@ DOCKER_IPS=$(sudo docker inspect $DOCKER_IDS|jq .[].NetworkSettings.IPAddress|se
 
 typeset -i INDEX=1;
 for DOCKER_IP in $DOCKER_IPS; do
- sed "s/<IP>/$DOCKER_IP/g ; s/<INDEX>/$INDEX/g" $BASE_DIR/docker_templates/tc_node.json| curl -s -XPUT 'http://127.0.0.1:8500/v1/catalog/register' -d @- >/dev/null
- sed "s/<IP>/$DOCKER_IP/g ; s/<INDEX>/$INDEX/g" $BASE_DIR/docker_templates/tc_svc.json| curl -s -XPUT 'http://127.0.0.1:8500/v1/agent/service/register' -d @- >/dev/null
+ sed "s/<IP>/$DOCKER_IP/g ; s/<INDEX>/$INDEX/g" $BASE_DIR/consul_templates/tc_node.json| curl -s -XPUT 'http://127.0.0.1:8500/v1/catalog/register' -d @- >/dev/null
+ sed "s/<IP>/$DOCKER_IP/g ; s/<INDEX>/$INDEX/g" $BASE_DIR/consul_templates/tc_svc.json| curl -s -XPUT 'http://127.0.0.1:8500/v1/agent/service/register' -d @- >/dev/null
  INDEX=$INDEX+1
 done
